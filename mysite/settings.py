cdcb7b69f2d1ab0b3eb1686a501146ea13823539
redjp <nya_um@yahoo.co.jp>
2019-09-25 14:35:45 +0900

    vs heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -154,7 +154,7 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    S3_URL = 'http://%s.s3-website-ap-northeast-1.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 
     MEDIA_URL = S3_URL
 
