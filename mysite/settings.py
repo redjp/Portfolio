a76be90536a5c2d73cc53cdccd2be4e614c25ec7
redjp <nya_um@yahoo.co.jp>
2019-10-23 17:21:15 +0900

    Revert "Changed AWS_S3_URL"
    This reverts commit 1a5770b609d63a3992dfd09f9c562a3cd1edd900.

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,7 +158,7 @@ if not DEBUG:
     }
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    S3_URL = 'https://%s.s3-ap-northeast-1.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 
     MEDIA_URL = S3_URL
 
